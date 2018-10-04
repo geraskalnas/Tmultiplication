2f082a25a9623b72f815dd60498fe5b1f4430454
geraskalnas <geraskalnas@gmail.com>
2018-10-04 17:32:45 +0300

    Fixed intSize function

--- main.cpp
+++ main.cpp
@@ -6,25 +6,22 @@ int intSize(int x);
 int main()
 {
    int a, b, c;
-   cin >> a >> b; cin.ignore();
+   cin >> a >> b >> c; cin.ignore();
    cout << " " << a << endl;
    cout << "*" << endl;
    cout << " " << b << endl;
    cout << "-" << endl;
-   intSize(a);
+   intSize(b);
+
    return 0;
 }
 int intSize(int x){
     int mo, y, i=1;
-    //cout <<1<<endl;
-    while (x > 0){
+    while (True){
         mo = pow(10, i);
         y = x / mo;
-        x -= y;
-        if(y<1){ cout << "l 0" << endl; break;}
-        else if(x < 10){ cout << i << "l " << i << endl; break;}
-        cout << "y" << y <<endl;
-        cout << "mo" << mo << endl;
+        if(y < 1){cout << "exit l 1" << endl; break;}
+        if(y < 10){cout << "exit l " << i+1 << endl; break;}
         i++;
     }
 }
