04951802048f825dec621bed4d37bb5e3caf547d
geraskalnas <geraskalnas@gmail.com>
2018-10-04 18:51:52 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -5,23 +5,41 @@ using namespace std;
 int intSize(int x);
 int main()
 {
-   int a, b, c;
+   int a, b, c, as, bs, cs;
    cin >> a >> b >> c; cin.ignore();
-   cout << " " << a << endl;
+   cout << " " << (a>b?a:b) << endl;
    cout << "*" << endl;
-   cout << " " << b << endl;
-   cout << "-" << endl;
-   intSize(b);
-
+   cout << " " << (a<b?a:b) << endl;
+   c=a*b;
+   as=intSize(a);
+   bs=intSize(b);
+   cs=intSize(c);
+   if(cs > as && cs > bs){
+       for(int i=0; i<cs; i++){
+           cout << "-";
+       }
+       cout << "-" << endl;
+   }else if(as>bs){
+       for(int i=0; i<as; i++){
+           cout << "-";
+       }
+       cout << "-" << endl;
+   }else{
+       for(int i=0; i<bs; i++){
+           cout << "-";
+       }
+       cout << "-" << endl;
+   }
+   cout << " " << c << endl;
    return 0;
 }
 int intSize(int x){
     int mo, y, i=1;
-    while (True){
+    while (true){
         mo = pow(10, i);
         y = x / mo;
-        if(y < 1){cout << "exit l 1" << endl; break;}
-        if(y < 10){cout << "exit l " << i+1 << endl; break;}
+        if(y < 1){return 1;}
+        if(y < 10){return i+1;}
         i++;
     }
 }
