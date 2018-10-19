98ba757fe4d4d9f674b0894b71adaa773029e539
geraskalnas <geraskalnas@gmail.com>
2018-10-19 16:25:01 +0300

    Added namespace

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include<iostream>
 #include<math.h>
 
-using namespace std;
+namespace tools {
+    int intSize(int x)
+    {
+        int mo, y, i = 1;
+        while (true) {
+            mo = pow(10, i);
+            y = x / mo;
+            if (y < 1) {
+                return 1;
+            }
+            if (y < 10) {
+                return i + 1;
+            }
+            i++;
+        }
+    }
+    int getIntFrom(int x, int choosen, int len)
+    {
+        int mo, y, z;
+        for(int i=0; i<choosen; i++) {
+            mo = pow(10, len-i);
+            y = x / mo;
+            z = x-(y*mo);
+        }
+        mo = pow(10, len-choosen);
+        y = z / mo;
+        return y;
+    }
+}
 
-int intSize(int x);
-int getIntFrom(int x, int choosen, int len);
+using namespace std;
+using namespace tools;
+
+//int intSize(int x);
+//int getIntFrom(int x, int choosen, int len);
 int main()
 {
     int a, b, c, as, bs, cs, t, tm;
@@ -58,30 +89,4 @@ int main()
 
     return 0;
 }
-int intSize(int x)
-{
-    int mo, y, i = 1;
-    while (true) {
-        mo = pow(10, i);
-        y = x / mo;
-        if (y < 1) {
-            return 1;
-        }
-        if (y < 10) {
-            return i + 1;
-        }
-        i++;
-    }
-}
-int getIntFrom(int x, int choosen, int len)
-{
-    int mo, y, z;
-    for(int i=0; i<choosen; i++) {
-        mo = pow(10, len-i);
-        y = x / mo;
-        z = x-(y*mo);
-    }
-    mo = pow(10, len-choosen);
-    y = z / mo;
-    return y;
-}
+
